fb2a4f6ff3e94006b186b9c2bd44d7e607b2ec39
lee <lee_jiazh@163.com>
2018-04-13 16:04:45 +0800

    fatal update

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,14 @@ package main
 
 import (
 	. ".."
-	"fmt"
-	"strings"
 )
 
 func main() {
-	article, _ := NewArticle("static/articles/大数据时代我国企业财务共享中心的优化.pdf")
+	/*article, _ := NewArticle("static/articles/大数据时代我国企业财务共享中心的优化.pdf")
 	sum := article.Summary()
 
 	fmt.Print(strings.Join(sum, "\n"))
 	keys := article.GetKeyWords()
-	fmt.Print(keys)
+	fmt.Print(keys)*/
 	RunServer()
 }
--- tika.go
+++ tika.go
@@ -3,6 +3,9 @@ package Posger
 import (
 	"os"
 	"log"
+	"os/exec"
+	"bytes"
+	"fmt"
 )
 
 var (
@@ -14,6 +17,21 @@ var (
 // 2. read tika config file.
 func init() {
 	//exec.Command("java -jar /home/lee/workspace/Posger2/tools/tika-server-1.17.jar")
+
+	go func() {
+		cmd := exec.Command("/home/lee/Programs/jdk1.8.0_161/bin/java", "-jar", "./tools/tika-server-1.17.jar")
+		var out bytes.Buffer
+
+		cmd.Stdout = &out
+		err := cmd.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s", out.String())
+	}()
+
+
+
 	tikaServerUrl = "http://localhost:9998"
 	log.Println("TIKA started at " + tikaServerUrl)
 }
